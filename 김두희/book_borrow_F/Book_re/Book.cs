6c7c637004b32c95281ba17b7e188427ea4fe13a
kimduhee7524 <duhee7524@naver.com>
2021-07-06 17:03:31 +0900

    .

---
+++ 김두희/book_borrow_F/Book_re/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_re
+{
+    public class Book
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }   // 제목
+        public string Author { get; set; }  // 저자
+        public string Publisher { get; set; }   // 출판사(발행자)
+        public string Year { get; set; }   // 발행연도
+
+
+
+        public bool IsBorrowed { get; set; }    //대여 가능 확인
+        public string RentDate { set; get; }    //대여일
+        public string ReturnPlanDate { set; get; }  //반납예정일
+        public string ReturnDate { set; get; }     //반납일
+
+    }
+}
---
+++ 김두희/book_borrow_F/Book_re/UI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book_re
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        DataManager instances = new DataManager();
+
+
+        private void bt_search_Click(object sender, EventArgs e)
+        {
+            instances.Load(txt_search.Text);
+            new BookSearcher(DataManager.searchbooks, DataManager.LoginDone).ShowDialog();
+
+            
+        }
+    }
+}
